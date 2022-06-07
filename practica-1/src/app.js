b639a21f81cb421b9690e25cd241e6b9901f1c41
FrancoL <francovalentin5005@gmail.com>
2022-06-07 19:30:30 -0300

    app backend

--- practica-1/src/app.js
+++ practica-1/src/app.js
@@ -6,12 +6,20 @@ const PORT = 3000;
 
 app.listen(PORT, () => {console.log("estoy vivo!")})
 
+// INDEX
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views/index.html"))
 })
+
+//LISTA DE PRODUCTOS
+
 app.get("/productos", (req, res) => {
     res.sendFile(path.join(__dirname, "views/productos.html"))
 })
-app.get("/productos/producto-1", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/productos/producto-1.html"))
+
+//PRODUCTOS N
+
+app.get("/productos/producto-:id", (req, res) => {
+    res.sendFile(path.join(__dirname, `views/productos/producto-${req.params.id}.html`))
 })
