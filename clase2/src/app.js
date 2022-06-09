276cbfd000cd6cb6f5edf15a8fc521fa4dc39760
FrancoL <francovalentin5005@gmail.com>
2022-06-09 20:33:41 -0300

    backend

---
+++ clase2/src/app.js
@@ -0,0 +1,12 @@
+"use strict"
+
+const express = require("express")
+const app = express();
+const path = require("path")
+app.listen(3000, () => console.log("estoy vivo"))
+
+
+app.get("/", (req, res)=>{
+    console.log("conectado a /")
+    res.sendFile(path.resolve("public/index.html"))
+})
