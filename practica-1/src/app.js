72da4743ad2fc2a5db2076e5932bc6c08bc537f9
FrancoL <francovalentin5005@gmail.com>
2022-06-14 19:22:14 -0300

    ejs practica

--- practica-1/src/app.js
+++ practica-1/src/app.js
@@ -4,22 +4,26 @@ const path = require("path")
 const app = express();
 const PORT = 3000;
 
+app.set("view engine", "ejs")
+
 app.listen(PORT, () => {console.log("estoy vivo!")})
 
 // INDEX
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/index.html"))
+    res.sendFile(path.join(__dirname,"../views/index.html"))
+    console.log("estoy en " + req.url)
 })
 
 //LISTA DE PRODUCTOS
 
 app.get("/productos", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/productos.html"))
+    res.render(`productos`, {url:req.url})
+    console.log("estoy en " + req.url)
 })
-
 //PRODUCTOS N
 
 app.get("/productos/producto-:id", (req, res) => {
-    res.sendFile(path.join(__dirname, `views/productos/producto-${req.params.id}.html`))
+    res.render(`productos/producto-${req.params.id}`, {url:req.url})
+    console.log("estoy en " + req.url)
 })
